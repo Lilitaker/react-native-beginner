0a0a1559c8f5e448850f8cd44277317b700c470c
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2023-04-24 11:17:16 -0500

    Add core components and styles to StartGameScreen

--- GuessTheNumber/screens/StartGameScreen.js
+++ GuessTheNumber/screens/StartGameScreen.js
@@ -1,9 +1,17 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game Screen!</Text>
+      <Text style={styles.title}>Start a new game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a number</Text>
+        <TextInput style={styles.input} />
+        <View style={styles.btnContainer}>
+          <Button title='Reset' onPress={() => {}} />
+          <Button title='Confirm' onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -13,6 +21,29 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  btnContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 8, //Android only
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
   },
 });
 
