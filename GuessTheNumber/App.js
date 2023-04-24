cac3f07b33790e3a5a5c39c61af3535ca82c3c7a
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2023-04-24 10:37:57 -0500

    Add startGameScreen component to app.js

--- GuessTheNumber/App.js
+++ GuessTheNumber/App.js
@@ -2,11 +2,13 @@ import { StyleSheet, View } from 'react-native';
 
 /* Components */
 import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title='Guess a number' />
+      <StartGameScreen />
     </View>
   );
 }
